125b70b21ee34c20c899df2de25b713ff2b29c82
dhfkdlsj <dhfkdlsj1@gmail.com>
2024-11-06 09:34:47 +0900

    Env : 초기 환경 구축

---
+++ src/main/java/miniproject/notiboard/NotiBoardApplication.java
@@ -0,0 +1,13 @@
+package miniproject.notiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotiBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miniproject/notiboard/NotiBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package miniproject.notiboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotiBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
